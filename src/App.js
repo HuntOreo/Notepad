b584379077cc2c3b46332cce94c3430af0848ba3
HuntOreo <hloreair@outlook.com>
2022-05-24 18:49:48 -0400

    added and incorporated react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Container>
+      <Row>
+        <Col>
+          <h1 className='col'>Hello World</h1>
+        </Col>
+      </Row>
+      </Container>
     </div>
   );
 }
