b3829a1526f6d24bafc8715928e2233c73487c48
HuntOreo <hloreair@outlook.com>
2022-05-24 20:29:00 -0400

    added env variable

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,28 @@
 const express = require('express')
 const app = express()
+const axios = require('axios');
+
+//.env middleware
+require('dotenv').config()
+
+const uri = process.env.URI
+
+
+
+//connect to mongo database
+
+
+
+app.get('/', async (req, res) => {
+    await getCollection('Test')
+    res.send('Welcome')
+})
+
+app.get('/users', (req, res) => {
+    res.send('Users')
+})
+
+app.get('/notes')
 
 app.listen(5000, () => {
     console.log('Server is listening....')
