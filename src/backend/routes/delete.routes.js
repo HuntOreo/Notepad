00a0fd601158b1850dbf040a94ca17b2b51d76a6
HuntOreo <hloreair@outlook.com>
2022-05-28 13:06:38 -0400

    can now delete user

--- src/backend/routes/delete.routes.js
+++ src/backend/routes/delete.routes.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const User = require('../schemas/users.schema')
 const Notepad = require('../schemas/notepads.schema')
 
-router.delete('/users/user/:id', async (req, res) => {
+router.delete('/user/:id', async (req, res) => {
     const user = await User.findByIdAndDelete(req.params.id)
-    console.log(user)
+    res.redirect('/')
 })
 
 module.exports = router
